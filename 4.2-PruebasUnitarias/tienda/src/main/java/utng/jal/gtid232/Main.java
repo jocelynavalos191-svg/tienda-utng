184932be0becbd84e93f3328e727a0c7d8d10a40
jocelynavalos191-svg <jocelynavalos191@gmail.com>
2026-08-25 10:03:59 -0600

    Delete 4.2-PruebasUnitarias/tienda directory

--- 4.2-PruebasUnitarias/tienda/src/main/java/utng/jal/gtid232/Main.java
+++
@@ -1,7 +0,0 @@
-package utng.jal.gtid232;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- 4.2-PruebasUnitarias/tienda/src/main/java/utng/jal/gtid232/dao/ProductoDAO.java
+++
@@ -1,9 +0,0 @@
-package utng.jal.gtid232.dao;
-
-public class ProductoDAO {
-
-    public double getPrecio() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-}
